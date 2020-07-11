606de808cdc774cf325488d179d79ecde0bdccbe
thierryiseli <me@thierryiseli.com>
2020-07-11 21:19:22 +0200

    Add microsoft_dev role and add configure ansible

--- installing.cmd
+++ installing.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 cinst Microsoft-Windows-Subsystem-Linux -source WindowsFeatures -y
+powershell.exe -ExecutionPolicy bypass -File ./ConfigureRemotingForAnsible.ps1
 ECHO "Please restart your computer and continue with next steps"
